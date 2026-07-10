332a9d8fc0e1d0affb1398480bb03aafb0e35fb5
Ahmed Elsifi <ahmedelsifi06@gmail.com>
2026-07-11 00:05:45 +0300

    Refactor file handling methods and enhance comments for clarity

--- Task1-Part1.js
+++ Task1-Part1.js
@@ -6,7 +6,27 @@ import fs from "fs";
 // or :
 // const fs = require("fs");
 
+// ======================================================
 // Method 1
+fs.writeFile(
+  "notes.txt",
+  "I am Ahmed Elsifi, and I work as a Software Engineer",
+  (error) => {
+    if (error) {
+      console.log(error);
+      return;
+    }
+
+    console.log("no errors");
+  },
+);
+
+// fs.writeFile(<file-path>,<file-content>,<callback-fn>);
+// it creates a new file or replaces the content of the file if a file with the same name already exists
+// also it receives a callback-fn with an error argument incase something happened
+
+// ======================================================
+// Method 2
 fs.readFile("notes.txt", "utf8", (error, data) => {
   if (error) {
     console.log(error);
@@ -19,8 +39,8 @@ fs.readFile("notes.txt", "utf8", (error, data) => {
 // fs.readFile(<file-path>,<encoding>,<callback-fn>); => used to read contents of a file, the callback-fn receives the contents and the error if there is any
 // we use "utf8" because data is stored as bytes of ASCII characters and we need a method to transform them from some numbers into characters
 
-// Method 2
-
+// ======================================================
 // Method 3
 
+// ======================================================
 // Method 4
