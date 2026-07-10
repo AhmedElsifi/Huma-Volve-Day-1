f871688f0966c3c385544264d545eb37df265a8e
Ahmed Elsifi <ahmedelsifi06@gmail.com>
2026-07-10 23:57:08 +0300

    Add initial implementation of file reading using fs module and create package.json

---
+++ Task1-Part1.js
@@ -0,0 +1,26 @@
+// Research :
+
+// "fs" stands for File System, since Node.js can't interact with files on the computer by itself it uses fs "file system library".
+// "fs" can be imported using :
+import fs from "fs";
+// or :
+// const fs = require("fs");
+
+// Method 1
+fs.readFile("notes.txt", "utf8", (error, data) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  console.log(data);
+});
+
+// fs.readFile(<file-path>,<encoding>,<callback-fn>); => used to read contents of a file, the callback-fn receives the contents and the error if there is any
+// we use "utf8" because data is stored as bytes of ASCII characters and we need a method to transform them from some numbers into characters
+
+// Method 2
+
+// Method 3
+
+// Method 4
